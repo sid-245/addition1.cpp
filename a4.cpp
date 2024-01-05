5d0f000796336d0fbe629d9c8f114e5cff62eea3
Siddhi@patil <118968670+sid-245@users.noreply.github.com>
2024-01-05 19:38:55 +0530

    Add files via upload

---
+++ a4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the num 1:-"<<endl;
+    cin>>a;
+    cout<<"enter the num 2:-";
+    cin>>b;
+    c=a+b;
+    cout<<"sum="<<c<<endl;
+    return 0;
+
+
+}
